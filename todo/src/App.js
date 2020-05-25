4f5beca8ae83c3a399c069e26a63b1d23fffd9cd
Joseph Nguyen <jonguyen@ucsd.edu>
2020-05-24 19:14:54 -0700

    CSS, Set up list, Event handler

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      content:'Do Laundry',
+      isDone: true,
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form className="todo_list">
+          <ul>
+            {todoList.map((todoItem,i) => (
+              <div className="todo_item">
+                <div className="checkbox" />
+                <input type="text" value={todoItem.content}/>
+                <div className="trash">
+                  <img
+                    className="trash_icon" 
+                    src={process.env.PUBLIC_URL + '/trash.png'}/> 
+                </div>
+              </div>
+            ))}
+          </ul>
+        </form>
+        <form className="add_todo">
+          <input type="text" id="text_field" placeholder="Add Todo"/>
+          <input type="button" 
+            onClick={() => {
+              const newTodoList = [...todoList];
+              newTodoList.push(
+                {
+                  content:'something else',
+                  isDone: true,
+                }
+              )
+              setTodoList(newTodoList)
+            }}
+            value="Add"
+            id="submit_button"/>
+        </form>
+      </div>
     </div>
   );
 }
